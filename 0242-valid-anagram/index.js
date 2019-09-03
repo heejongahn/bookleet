760489f2b11c18bb922d2d5c056d2f7c85e87f09
Heejong Ahn <heejongahn@gmail.com>
2019-09-03 15:30:22 +0900

    Solve 0242-valid-anagram follow-up question

--- 0242-valid-anagram/index.js
+++ 0242-valid-anagram/index.js
@@ -1,10 +1,10 @@
 // https://leetcode.com/problems/valid-anagram/
-
 /**
  * @param {string} s
  * @param {string} t
  * @return {boolean}
  */
+
 const isAnagram = function(s, t) {
   const minCharCode = "a".charCodeAt(0);
   const maxCharCode = "z".charCodeAt(0);
@@ -24,4 +24,30 @@ const isAnagram = function(s, t) {
   }
 
   return !countArray.some(v => v !== 0);
+};
+
+const isAnagram_followUp = function(s, t) {
+  const countMap = {};
+
+  for (const char of s) {
+    const beforeCount = countMap[char];
+    countMap[char] = beforeCount === undefined ? 1 : beforeCount + 1;
+  }
+
+  for (const char of t) {
+    const beforeCount = countMap[char];
+    if (beforeCount === undefined) {
+      return false;
+    }
+
+    countMap[char] = beforeCount - 1;
+  }
+
+  for (const count of Object.values(countMap)) {
+    if (count !== 0) {
+      return false;
+    }
+  }
+
+  return true;
 };
