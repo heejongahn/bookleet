5dee8ae346d2d47f9b3a91558d973c20a1b58403
Heejong Ahn <heejongahn@gmail.com>
2019-09-03 15:26:23 +0900

    Solve 0242-valid-anagram

---
+++ 0242-valid-anagram/index.js
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/valid-anagram/
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+const isAnagram = function(s, t) {
+  const minCharCode = "a".charCodeAt(0);
+  const maxCharCode = "z".charCodeAt(0);
+
+  const charCodeRange = maxCharCode - minCharCode + 1;
+
+  const countArray = Array.from({ length: charCodeRange }).map(_ => 0);
+
+  for (const char of s) {
+    const index = char.charCodeAt(0) - minCharCode;
+    countArray[index] = countArray[index] + 1;
+  }
+
+  for (const char of t) {
+    const index = char.charCodeAt(0) - minCharCode;
+    countArray[index] = countArray[index] - 1;
+  }
+
+  return !countArray.some(v => v !== 0);
+};
