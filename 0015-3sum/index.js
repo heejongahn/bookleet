9b15831378564c3c10fb65ae3d769f7a909ffd15
Heejong Ahn <heejongahn@gmail.com>
2019-09-05 16:57:24 +0900

    Solve 0015-3sum

--- 0015-3sum/index.js
+++ 0015-3sum/index.js
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/3sum/
+
 function hasAppeared(map, threeSum) {
   const [first, second, third] = threeSum;
 
@@ -53,6 +55,64 @@ const threeSum_TLE = function(rawNums) {
           results.push(newThreeSum);
           addAppearance(appearanceMap, newThreeSum);
         }
+      }
+    }
+  }
+
+  return results;
+};
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+const threeSum = function(rawNums) {
+  const nums = rawNums.sort((a, b) => a - b);
+  const length = nums.length;
+
+  const results = [];
+
+  for (let i = 0; i < length - 2; i++) {
+    const target = nums[i];
+
+    // As `results` is sorted
+    if (target > 0) {
+      continue;
+    }
+
+    // Avoid duplicatation within same `target`s with different `i` values
+    if (i > 0 && target === nums[i - 1]) {
+      continue;
+    }
+
+    let j = i + 1;
+    let k = length - 1;
+
+    while (j < k) {
+      const first = nums[j];
+      const second = nums[k];
+
+      const sum = target + first + second;
+
+      if (sum === 0) {
+        const result = [target, first, second];
+        results.push(result);
+
+        j++;
+        k--;
+
+        // Avoid duplications within same `i` value
+        while (j < k && nums[j] === nums[j - 1]) {
+          j++;
+        }
+
+        while (j < k && nums[k] === nums[k + 1]) {
+          k--;
+        }
+      } else if (sum > 0) {
+        k--;
+      } else {
+        j++;
       }
     }
   }
