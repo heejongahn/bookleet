3f707aca5237543717965716e3ae162decf7b3e1
Heejong Ahn <heejongahn@gmail.com>
2019-09-30 15:43:03 +0900

    Solve 0300-longest-increasing-subsequence with nlogn complexity

--- 0300-longest-increasing-subsequence/index.js
+++ 0300-longest-increasing-subsequence/index.js
@@ -24,4 +24,51 @@ const lengthOfLIS_nSquare = function(nums) {
   return Math.max(...lengthLIS);
 };
 
-const lengthOfLIS = lengthOfLIS_nSquare;
+function binarySearch(sequence, target) {
+  let start = 0;
+  let end = sequence.length - 1;
+  let mid = Math.floor((start + end) / 2);
+
+  while (start < end) {
+    const midValue = sequence[mid];
+
+    if (midValue < target) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+
+    mid = Math.floor((start + end) / 2);
+  }
+
+  return sequence[mid] >= target ? mid : mid + 1;
+}
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const lengthOfLIS_nLogN = function(nums) {
+  const length = nums.length;
+
+  if (length === 0) {
+    return 0;
+  }
+
+  const piles = [nums[0]];
+
+  for (let i = 1; i < length; i++) {
+    const num = nums[i];
+    const index = binarySearch(piles, num);
+
+    if (index >= piles.length) {
+      piles.push(num);
+    } else {
+      piles[index] = num;
+    }
+  }
+
+  return piles.length;
+};
+
+const lengthOfLIS = lengthOfLIS_nLogN;
