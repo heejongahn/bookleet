47af90fae785dcb8f4d4fbc2ef316cdd12477a04
Heejong Ahn <heejongahn@gmail.com>
2019-10-23 19:54:19 +0900

    Add two pointer solutions for trapping rain water

--- 0042-trapping-rain-water/index.js
+++ 0042-trapping-rain-water/index.js
@@ -34,3 +34,39 @@ var trap = function(height) {
 
   return sum;
 };
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap_twoPointer = function(height) {
+  let left = 0;
+  let right = height.length - 1;
+
+  let leftMax = height[left];
+  let rightMax = height[right];
+
+  let sum = 0;
+
+  while (left < right) {
+    if (height[left] < height[right]) {
+      if (leftMax < height[left]) {
+        leftMax = height[left];
+      } else {
+        sum += (leftMax - height[left]);
+      }
+
+      left += 1;
+    } else {
+      if (rightMax < height[right]) {
+        rightMax = height[right];
+      } else {
+        sum += (rightMax - height[right];
+      }
+
+      right -= 1;
+    }
+  }
+
+  return sum;
+};
